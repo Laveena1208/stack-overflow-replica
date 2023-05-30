30365b6cf0ac9f2a7e6e3219ebcd9fe39d032be8
Laveena1208 <2019laveena.kithani@ves.ac.in>
2023-05-30 20:37:02 +0530

    Initialize Project

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Question;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory(10)->create()->each(function($user){
+            for($i=0;$i<random_int(5,10);$i++){
+                $user->question()->create(
+                    Question::factory()
+                    ->make()
+                    ->toArray()
+                );
+            }
+        });
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
