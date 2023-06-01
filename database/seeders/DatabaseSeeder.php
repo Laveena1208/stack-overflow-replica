7abfe5d90550e2d06b12e32124c72e4c0c6d1af5
Laveena1208 <2019laveena.kithani@ves.ac.in>
2023-06-01 21:00:35 +0530

    updated user factory function to saveMany

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Answer;
 use App\Models\Question;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,14 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create()->each(function($user){
-            for($i=0;$i<random_int(5,10);$i++){
-                $user->question()->create(
-                    Question::factory()
-                    ->make()
-                    ->toArray()
+        \App\Models\User::factory(10)->create()->each(function ($user) {
+            $user->questions()->saveMany(
+                Question::factory(random_int(5, 15))->make()
+            )->each(function ($question) {
+                $question->answers()->saveMany(
+                    Answer::factory(random_int(2, 10))->make()
                 );
-            }
+            });
         });
 
         \App\Models\User::factory()->create([
@@ -30,8 +31,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'laveena@example.com',
             'password' => Hash::make('abcd1234')
         ]);
-
-
-
     }
 }
