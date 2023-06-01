144191167f0411ee8330158026c81ac7fca5efeb
Laveena1208 <2019laveena.kithani@ves.ac.in>
2023-06-01 19:37:50 +0530

    added Policy for update and delete question

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,6 +6,7 @@ namespace App\Providers;
 
 use App\Models\Question;
 use App\Models\User;
+use App\Policies\QuestionPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -17,7 +18,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        //
+        Question::class =>QuestionPolicy::class
     ];
 
     /**
@@ -25,13 +26,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('update-question', function(User $user, Question $question){
-            return $user->id === $question->user_id;
-        });
+        // Gate Method:-
+        // Gate::define('update-question', function(User $user, Question $question){
+        //     return $user->id === $question->user_id;
+        // });
 
-        Gate::define('delete-question', function(User $user, Question $question){
-            return $user->id === $question->user_id;
-        });
+        // Gate::define('delete-question', function(User $user, Question $question){
+        //     return $user->id === $question->user_id;
+        // });
 
     }
 }
