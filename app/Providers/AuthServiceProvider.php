679acfce2160aafaba9a9ce74b69af3860637a64
Laveena1208 <2019laveena.kithani@ves.ac.in>
2023-06-01 19:03:13 +0530

    added Gate for update and delete question

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\Question;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +25,13 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('update-question', function(User $user, Question $question){
+            return $user->id === $question->user_id;
+        });
+
+        Gate::define('delete-question', function(User $user, Question $question){
+            return $user->id === $question->user_id;
+        });
+
     }
 }
