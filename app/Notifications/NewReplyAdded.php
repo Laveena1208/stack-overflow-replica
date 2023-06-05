f5e2fe71003ae39c057c8d2c2ad786a8c3da95b3
Laveena1208 <2019laveena.kithani@ves.ac.in>
2023-06-06 00:19:37 +0530

    created notifications file by cmd

---
+++ app/Notifications/NewReplyAdded.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Question;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewReplyAdded extends Notification
+{
+    use Queueable;
+
+    private $question;
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Question $question)
+    {
+        $this->question = $question;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+                    ->line('A new reply added')
+                    ->action('View question', url($this->question->url))
+                    ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'question' =>$this->question
+        ];
+    }
+}
